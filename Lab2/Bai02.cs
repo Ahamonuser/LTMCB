c0c38d751f332e6595231b5d0c5f06ff57552666
Ahamonuser <116050616+Ahamonuser@users.noreply.github.com>
2023-04-17 09:41:32 +0700

    Add files via upload

---
+++ Lab2/Bai02.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TestLab2
+{
+    public partial class Bai02 : Form
+    {
+        public Bai02()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.ShowDialog();
+            FileStream fs = new FileStream(openFile.FileName, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(fs);
+            txtFileContent.Text = reader.ReadToEnd();
+            ShowFileName(openFile.SafeFileName);
+            ShowSize(fs.Length);
+            ShowURL(openFile.FileName);
+            ShowLineCount(fs);
+            ShowWordsCount(fs);
+            ShowCharacterCount(fs);
+            reader.Close();
+        }
+
+        void ShowFileName(string filename)
+        {
+            txtFileName.Text = filename;
+        }
+
+        void ShowSize(long size)
+        {
+            if (size > 1)
+            {
+                txtSize.Text = size.ToString() + " bytes";
+            } 
+            else
+            {
+                txtSize.Text = size.ToString() + " byte";
+            }
+        }
+
+        void ShowURL(string url)
+        {
+            txtURL.Text = url;
+        }
+
+        void ShowLineCount(FileStream fs)
+        {
+            fs.Position = 0;
+            StreamReader reader = new StreamReader(fs);
+            int linecount = 0;
+            while (reader.ReadLine() != null)
+            {
+                linecount++;
+            }
+            txtLineCount.Text = linecount.ToString();
+        }
+
+        void ShowWordsCount(FileStream fs)
+        {
+            fs.Position = 0;
+            StreamReader reader = new StreamReader(fs);
+            int wordscount = 0;
+            bool wordcounted = false;
+            while (reader.Read() != -1)
+            {
+                if (reader.Peek() == ' ' || reader.Peek() == '\n' || reader.Peek() == -1 || reader.Peek() == '\t')
+                {
+                    if (wordcounted == false)
+                    {
+                        wordscount++;
+                        wordcounted = true;
+                    }
+                    else
+                    {
+                        reader.Read();
+                        wordcounted = true;
+                    }
+                }
+                else
+                {
+                    wordcounted = false;
+                } 
+            }
+            txtWordsCount.Text = wordscount.ToString();
+        }
+
+        void ShowCharacterCount(FileStream fs)
+        {
+            fs.Position = 0;
+            StreamReader reader = new StreamReader(fs);
+            int charactercount = 0;
+            while (reader.Read() != -1)
+            {
+                charactercount++;
+            }
+            txtCharCount.Text = charactercount.ToString();
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Lab2/Bai03.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace TestLab2
+{
+    partial class Bai03
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFileContent = new System.Windows.Forms.RichTextBox();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFileContent
+            // 
+            this.txtFileContent.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFileContent.Location = new System.Drawing.Point(222, 29);
+            this.txtFileContent.Name = "txtFileContent";
+            this.txtFileContent.Size = new System.Drawing.Size(767, 310);
+            this.txtFileContent.TabIndex = 5;
+            this.txtFileContent.Text = "";
+            // 
+            // btnRead
+            // 
+            this.btnRead.AutoSize = true;
+            this.btnRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRead.Location = new System.Drawing.Point(22, 29);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(187, 91);
+            this.btnRead.TabIndex = 4;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.AutoSize = true;
+            this.btnWrite.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWrite.Location = new System.Drawing.Point(22, 147);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(187, 91);
+            this.btnWrite.TabIndex = 3;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // Bai03
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 360);
+            this.Controls.Add(this.txtFileContent);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnWrite);
+            this.Name = "Bai03";
+            this.Text = "Bai3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtFileContent;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnWrite;
+    }
+}
